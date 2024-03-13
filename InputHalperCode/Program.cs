e27289a22acf89ff4ca23a00641b99af25e65d02
Александр Александр <stripalol@inbox.ru>
2024-03-13 11:05:49 +0700

    Добавьте файлы проекта.

---
+++ InputHalperCode/Program.cs
@@ -0,0 +1,22 @@
+﻿public static class InputHelper
+{
+    public static bool Input(string _text, int _min, int _max, out int inputValue)
+    {
+        bool result = false;
+        Console.WriteLine(_text);
+        string InputValue = Console.ReadLine();
+        inputValue = int.Parse(InputValue);
+
+        if (inputValue >= _min || inputValue <= _max)
+        {
+            Console.WriteLine("Твое число " + inputValue);
+            result = true;
+        }
+        else
+        {
+            Console.WriteLine("Ошибка");
+            result = true;
+        }
+        return result;
+    }
+}
